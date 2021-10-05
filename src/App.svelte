1360348099ce591d9ed9d851a7e2217135e00fa3
m.ali petek <malipetek@gmail.com>
2021-10-05 03:42:32 +0300

    initial fixes

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,30 @@
 <script>
+	import { onMount } from 'svelte';
+  import JSONTree from 'svelte-json-tree';
 	export let NL_OS = 'Unknown OS';
 	export let NL_PORT = 'Unknown port';
 	export let Neutralino;
-	let settings = {};
-	Neutralino.settings.getSettings(function (settingsData) {
-			settings = settingsData;
-	});
+
 </script>
+
+<img src="svelte-logo-horizontal.svg" alt="Svelte Logo">
+<h1>Hi: {NL_OS} @ { NL_PORT } </h1>
+
+{#await Neutralino.app.getConfig()}
+	<p> Getting config... </p>
+{:then settings}
+<div style="--json-tree-string-color: yellowgreen; --json-tree-label-color: coral; --json-tree-number-color: cornflower; --json-tree-boolean-color: yellow">
+	<JSONTree value={settings} />
+</div>
+{/await}
 
 <style>
 	:global(body) {
 			background-color: black;
 			color: yellowgreen;
+			padding: 2em;
 	}
 	h1 {
-	color: yellowgreen;
+		color: yellowgreen;
 }
 </style> 
-<img src="/assets/svelte-logo-horizontal.svg" alt="Svelte Logo">
-<h1>Hi: {NL_OS} @ { NL_PORT } </h1>
-<p> { JSON.stringify(settings) } </p>
